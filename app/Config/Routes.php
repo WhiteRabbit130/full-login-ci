02f0b802ca5f2919cfc396ee7a5217604f2a6ea1
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-01-31 21:38:22 +0200

    make home and about simple pages

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,8 @@ use App\Controllers\Pages;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
 $routes->get('pages', [Pages::class, 'index']);
+
 $routes->get('(:segment)', [Pages::class, 'view']);
 
---
+++ app/Controllers/Pages.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function view($page = 'home')
+    {
+        if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
+    }
+}
